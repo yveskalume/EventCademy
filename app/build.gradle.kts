cdb38c46940281f4157c7fed7d41a061fd5c58b9
yveskalume <yvkalume@gmail.com>
2023-08-04 21:19:52 +0200

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,12 +11,12 @@ plugins {
 
 android {
     namespace = "com.yvkalume.eventcademy"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.yvkalume.eventcademy"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
