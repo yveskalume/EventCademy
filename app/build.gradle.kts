cc97cf75b69981d8575ba0aeb0f83fb72e4c62bd
yveskalume <yvkalume@gmail.com>
2023-09-05 21:20:25 +0200

    increase app versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.yveskalume.eventcademy"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1"
+        versionCode = 6
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
